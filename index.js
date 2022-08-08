bee565a00bce91a98530b2afe1b92c9968d22252
Arinze <calistusarinzechinweuba@gmail.com>
2022-08-08 09:50:09 +0100

    Deleted irrelavant materials

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const whatsInputed = prompt("Enter anything");
 
-if (whatsInputed === String){
-   console.log("You just entered a string")
-}
-// else if( whatsInputed === Number){
-//    console.log("You just entered a Number")
-// }
-else{
-   console.log("You just entered a Number")
-}
 
 
 
