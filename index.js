7b07eb2837fdf43351c427676702514eb74b7937
Arinze <calistusarinzechinweuba@gmail.com>
2022-08-08 08:47:16 +0100

    Added an animation into my project section

---
+++ index.js
@@ -0,0 +1,14 @@
+const whatsInputed = prompt("Enter anything");
+
+if (whatsInputed === String){
+   console.log("You just entered a string")
+}
+// else if( whatsInputed === Number){
+//    console.log("You just entered a Number")
+// }
+else{
+   console.log("You just entered a Number")
+}
+
+
+
