1a602db77a09bb1be4e89338f92f5dc6524b6b6d
Arinze <calistusarinzechinweuba@gmail.com>
2022-08-15 18:08:01 +0100

    Add Dark-Mode

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-
+// Dark Mode Effect
+const options = {
+   bottom: '32rem', // default: '32px'
+   right: '30px', // default: '32px'
+   left: 'unset', // default: 'unset'
+   time: '0.5s', // default: '0.3s'
+   mixColor: '#fff', // default: '#fff'
+   backgroundColor: '#fff',  // default: '#fff'
+   buttonColorDark: '#100f2c',  // default: '#100f2c'
+   buttonColorLight: '#fff', // default: '#fff'
+   saveInCookies: true, // default: true,
+   label: '🌓', // default: ''
+   autoMatchOsTheme: true // default: true
+ }
+ 
+ const darkmode = new Darkmode(options);
+ darkmode.showWidget();
 
 
 
